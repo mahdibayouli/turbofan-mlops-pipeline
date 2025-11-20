2575c0806dd77e32048fbc2c82fa2c1f3ebabb5f
mahdibayouli <bayoulimahdi1@gmail.com>
2025-11-20 18:23:03 +0100

    refactor: centralize dataset paths and column definitions under src/config.py and update exploration notebook to use it

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+REPO_ROOT = Path(__file__).resolve().parents[1] # src/ is one level below root
+
+# Dataset
+DATA_DIR = REPO_ROOT / "data" / "CMAPSSData"
+TRAIN_FD001_PATH = DATA_DIR / "train_FD001.txt"
+
+# Columns definitions
+OP_SETTINGS = [f"op_setting_{i}" for i in range(1, 4)] 
+SENSORS = [f"sensor_{i}" for i in range(1, 22)]
+COLS = ["unit_number", "time_in_cycles"] + OP_SETTINGS + SENSORS
+
+# Artifacts
+ARTIFACTS_DIR = REPO_ROOT / "models"
+SCALER_PATH = ARTIFACTS_DIR / "scaler.joblib"
+ARTIFACTS_INFO_PATH = ARTIFACTS_DIR / "artifacts_info.json"
